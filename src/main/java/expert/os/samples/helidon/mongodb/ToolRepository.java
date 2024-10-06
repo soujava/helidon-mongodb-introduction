1bd3e2112bb996b2d3de1eb99eb605bc361d312a
Otavio Santana <otaviopolianasantana@gmail.com>
2024-10-06 20:29:05 +0100

    feat: create tool repository
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/expert/os/samples/helidon/mongodb/ToolRepository.java
+++ src/main/java/expert/os/samples/helidon/mongodb/ToolRepository.java
@@ -1,2 +1,8 @@
-package expert.os.samples.helidon.mongodb;public interface ToolRepository {
+package expert.os.samples.helidon.mongodb;
+
+import jakarta.data.repository.BasicRepository;
+import jakarta.data.repository.Repository;
+
+@Repository
+public interface ToolRepository extends BasicRepository<Tool, String> {
 }
